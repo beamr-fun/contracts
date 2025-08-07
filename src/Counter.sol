557e446592c6bda593da4d245bc17d4c1ba71933
jordan <jordan.lesich@gmail.com>
2025-08-06 18:48:49 -0700

    install SF monorepo

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,4 +1,12 @@
 // SPDX-License-Identifier: UNLICENSED
+
+import {
+    PoolConfig,
+    PoolERC20Metadata
+} from
+    "@superfluid/ethereum-contracts/contracts/interfaces/agreements/gdav1/IGeneralDistributionAgreementV1.sol";
+
+
 pragma solidity ^0.8.23;
 
 contract BeamR {
@@ -6,6 +14,12 @@ contract BeamR {
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
+
+        PoolERC20Metadata memory poolMD = PoolERC20Metadata({
+            name: "BeamR Pool",
+            symbol: "BRP",
+            decimals: 18
+        });
     }
 
     function increment() public {
