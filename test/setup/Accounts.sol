5ab983141efca1c52aed16ca8562951a19fcc927
jordan <jordan.lesich@gmail.com>
2025-08-07 13:41:53 -0700

    accounts

---
+++ test/setup/Accounts.sol
@@ -0,0 +1,58 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.23;
+
+import "forge-std/StdCheats.sol";
+
+contract Accounts is StdCheats {
+    // //////////////////////
+    // Vote Admins
+    // //////////////////////
+
+    function dummyDao() public returns (address) {
+        return makeAddr("dummy_dao");
+    }
+
+    function admin1() public returns (address) {
+        return makeAddr("admin_1");
+    }
+
+    function admin2() public returns (address) {
+        return makeAddr("admin_2");
+    }
+
+    function admin3() public returns (address) {
+        return makeAddr("admin_3");
+    }
+
+    // //////////////////////
+    // Users
+    // //////////////////////
+
+    function user1() public returns (address) {
+        return makeAddr("user_1");
+    }
+
+    function user2() public returns (address) {
+        return makeAddr("user_2");
+    }
+
+    function user3() public returns (address) {
+        return makeAddr("user_3");
+    }
+
+    function user4() public returns (address) {
+        return makeAddr("user_4");
+    }
+
+    function user5() public returns (address) {
+        return makeAddr("user_5");
+    }
+
+    // //////////////////////
+    // Outsiders
+    // //////////////////////
+
+    function someGuy() public returns (address) {
+        return makeAddr("some_guy");
+    }
+}
