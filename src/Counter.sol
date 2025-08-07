74f1796dc8ac92ac8b18a5e76fdd61dbe5e0a47d
jordan <jordan.lesich@gmail.com>
2025-08-06 19:28:10 -0700

    pretty sure deps are handled

--- src/Counter.sol
+++ src/Counter.sol
@@ -1,30 +1,29 @@
 // SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
 
 import {
     PoolConfig,
     PoolERC20Metadata
-} from
-    "@superfluid/ethereum-contracts/contracts/interfaces/agreements/gdav1/IGeneralDistributionAgreementV1.sol";
+} from "@superfluid/ethereum-contracts/contracts/interfaces/agreements/gdav1/IGeneralDistributionAgreementV1.sol";
+
+import {SuperTokenV1Library} from "@superfluid/ethereum-contracts/contracts/apps/SuperTokenV1Library.sol";
 
 import {
     ISuperfluidPool,
     ISuperToken
 } from "@superfluid/ethereum-contracts/contracts/interfaces/superfluid/ISuperfluid.sol";
 
-
-pragma solidity ^0.8.23;
-
 contract BeamR {
+    using SuperTokenV1Library for ISuperToken;
+
     uint256 public number;
 
     function setNumber(uint256 newNumber) public {
         number = newNumber;
 
-        PoolERC20Metadata memory poolMD = PoolERC20Metadata({
-            name: "BeamR Pool",
-            symbol: "BRP",
-            decimals: 18
-        });
+        PoolERC20Metadata memory poolMD = PoolERC20Metadata({name: "BeamR Pool", symbol: "BRP", decimals: 18});
+
+        ISuperfluidPool pool = ISuperfluidPool(address(this));
     }
 
     function increment() public {
